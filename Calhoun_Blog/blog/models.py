66f5ee16913edb1eeb2cfdf62990bf3c449a1474
Akbar Pathan <akbar.pathan034@gmail.com>
2024-04-06 04:39:39 -0400

    Added the ability to comment

--- Calhoun_Blog/blog/models.py
+++ Calhoun_Blog/blog/models.py
@@ -8,5 +8,15 @@ class Post(models.Model):
     published_date = models.DateTimeField(auto_now_add=True)
     # Optional: add more fields as needed, such as `categories` or `tags`
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text[:50]  # Show first 50 characters of the comment
+
 def __str__(self):
     return self.title
